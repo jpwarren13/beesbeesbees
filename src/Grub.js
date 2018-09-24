1fb6ddda7997d3b0744c4632a6000b1dd3202c28
johnpwarren@gmail.com <johnpwarren@gmail.com>
2018-09-24 10:20:26 -0700

    grub working a bit.

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,74 @@
-var Grub = function() {
-};
 
+function Grub() {
+
+        this.age = 0;
+        this.color = 'pink';
+        this.food = 'jelly';
+    return this;
+
+    };
+Grub.prototype.eat = function(){
+ return 'Mmmmmmmmm ' + this.food;
+      }
+
+
+
+function Bee(){
+            Grub.call(this);
+            this.age = 5;
+            this.color = 'yellow';
+            this.job = 'Keep on growing';
+        }
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
+
+function HoneyMakerBee () {
+        Bee.call(this);
+        this.age = 10;
+        this.job = 'make honey';
+        this.honeyPot = 0;
+    return this;
+    };
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+    makeHoney(){
+        this.honeyPot++;
+    }
+    giveHoney(){
+        this.honeyPot--;
+    }
+
+}
+
+class ForagerBee extends Bee {
+    constructor(){
+        super();
+        this.age = 10;
+        this.job = 'find pollen';
+        this.canFly = true;
+        this.treasureChest = [];
+    }
+    forage(treasure){
+        this.treasureChest.push(treasure);
+    }
+}
+
+class RetiredForagerBee extends ForagerBee {
+    constructor(){
+        super();
+        this.age = 40;
+        this.job = 'gamble';
+        this.canFly = false;
+        this.color = 'grey';
+        
+    }
+    forage(){
+        return 'I am too old, let me play cards instead';
+    }
+    gamble(treasure){
+    this.treasureChest.push(treasure);
+    }
+}
